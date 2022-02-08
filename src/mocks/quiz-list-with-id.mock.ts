f5037d1bde25b4c46008886868aab713f1978beb
NablaT <remi.pourtier@gmail.com>
2022-02-08 10:51:14 +0100

    Adding quiz mock with id

---
+++ src/mocks/quiz-list-with-id.mock.ts
@@ -0,0 +1,78 @@
+[
+    {
+        "name": "Les Acteurs",
+        "theme": "Actor",
+        "id": "1",
+        "questions": [
+            {
+                "label": "Jean Gabin a joué dans...",
+                "answers": [
+                    {
+                        "value": "Les tuches II",
+                        "isCorrect": false
+                    },
+                    {
+                        "value": "La grande illusion",
+                        "isCorrect": true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Les sports",
+        "theme": "Sport",
+        "id": "2",
+        "questions": [
+            {
+                "label": "Aux echecs, Magnus Carlsen est champion du monde depuis...",
+                "answers": [
+                    {
+                        "value": "2013",
+                        "isCorrect": true
+                    },
+                    {
+                        "value": "2015",
+                        "isCorrect": false
+                    },
+                    {
+                        "value": "2017",
+                        "isCorrect": false
+                    },
+                    {
+                        "value": "2019",
+                        "isCorrect": false
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Les memes",
+        "theme": "Fun",
+        "id": "3",
+        "questions": [
+            {
+                "label": "Completez cette phrase: Hide the Pain...",
+                "answers": [
+                    {
+                        "value": "Hugo",
+                        "isCorrect": false
+                    },
+                    {
+                        "value": "Joe",
+                        "isCorrect": false
+                    },
+                    {
+                        "value": "Bernard",
+                        "isCorrect": false
+                    },
+                    {
+                        "value": "Harold",
+                        "isCorrect": true
+                    }
+                ]
+            }
+        ]
+    }
+]
